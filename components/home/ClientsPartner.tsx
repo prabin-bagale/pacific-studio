a66851ca23af83ce587f6a8bece4a72235f508b7
prabin bagale <prabinbagale08@gmail.com>
2025-03-04 23:19:38 +0545

    fix lint

--- components/home/ClientsPartner.tsx
+++ components/home/ClientsPartner.tsx
@@ -1,14 +1,13 @@
 import { FC } from 'react';
 
-
 const ClientsPartner: FC =() =>{
     return(
         <section className="container">
       <div className="flex flex-col gap-2 items-center text-pure-white justify-center mb-10">
         <h2 className="text-center text-title-2 font-bold mb-4">Our Clients And Parnters </h2>
-        
       </div>
     </section>
     )
 }
+
 export default ClientsPartner;
